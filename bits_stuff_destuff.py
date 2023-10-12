0943df3448d7cd0262f5d46d69972b8c5d0241b4
Akibur Rahman <me.akiburrahman@gmail.com>
2023-10-12 13:03:08 +0600

    update

--- bits_stuff_destuff.py
+++ bits_stuff_destuff.py
@@ -1,9 +1,12 @@
 flag = input("Enter Flag:")
 data = input("Enter Data:")
+
 fiveOneCount=data.count('11111')
+
 stuffed=data.replace('11111','111110',fiveOneCount)
 stuffed=flag+stuffed+flag
 print(stuffed)
+
 destuffed=stuffed[len(flag):len(stuffed)-len(flag)]
 destuffed=destuffed.replace('111110','11111')
 print(destuffed)
