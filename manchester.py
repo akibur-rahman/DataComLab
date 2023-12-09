7e1978ee794d1372c330e735e46e9ac7b965d1bc
Akibur Rahman <me.akiburrahman@gmail.com>
2023-12-09 15:01:54 -0800

    Update manchester.py

--- manchester.py
+++ manchester.py
@@ -1,31 +1,45 @@
-# Write code for manchester encoding, take data form user. plot graph.
-
 import matplotlib.pyplot as plt
-import numpy as np
 
 
-def manchester(data):
-    manchester = []
-    for i in range(len(data)):
-        if data[i] == 0:
-            manchester.append(1)
-            manchester.append(-1)
-        else:
-            manchester.append(-1)
-            manchester.append(1)
-    return manchester
+def manchester_encode(data):
+    encoded_data = []
+    for bit in data:
+        if bit == '0':
+            # Represents a transition from high to low
+            encoded_data.extend([1, -1])
+        elif bit == '1':
+            # Represents a transition from low to high
+            encoded_data.extend([-1, 1])
+    return encoded_data
 
 
-def plot_graph(data, manchester):
-    x = np.arange(0, len(data), 0.5)
-    plt.plot(x, data, drawstyle='steps-pre', label='NRZ')
-    plt.plot(x, manchester, drawstyle='steps-pre', label='Manchester')
+def plot_manchester_encoding(data, encoded_data):
+    time_values = range(0, 2 * len(data), 2)
+    time_values_encoded = [val for pair in zip(
+        time_values, time_values) for val in pair][:len(encoded_data)]
+
+    plt.step(time_values_encoded, encoded_data,
+             where='post', label='Manchester Encoding')
+    plt.xticks(time_values, list(data))
+    plt.title('Manchester Encoding')
+    plt.xlabel('Time')
+    plt.ylabel('Signal Level')
     plt.legend()
     plt.show()
 
 
 def main():
-    data = input("Enter data: ")
-    data = list(map(int, data))
-    manchester_data = manchester(data)
-    plot_graph(data, manchester_data)
+    user_data = input("Enter binary data (e.g., 101010): ")
+    # Remove non-binary characters
+    user_data = ''.join(filter(lambda x: x in {'0', '1'}, user_data))
+
+    if not user_data:
+        print("Invalid input. Please enter valid binary data.")
+        return
+
+    encoded_data = manchester_encode(user_data)
+    plot_manchester_encoding(user_data, encoded_data)
+
+
+if __name__ == "__main__":
+    main()
