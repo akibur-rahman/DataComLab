f832821b53558cc9f39a5fd2bf37b1634328a6e7
Akibur Rahman <me.akiburrahman@gmail.com>
2023-12-09 14:51:58 -0800

    Update manchester.py

--- manchester.py
+++ manchester.py
@@ -1,49 +1,31 @@
-# Write code for manchester encoding and decoding, take data form user. plot graph for both.
+# Write code for manchester encoding, take data form user. plot graph.
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-def manchester_encode(data):
-    encoded_data = []
+def manchester(data):
+    manchester = []
     for i in range(len(data)):
         if data[i] == 0:
-            encoded_data.append(-1)
-            encoded_data.append(1)
+            manchester.append(1)
+            manchester.append(-1)
         else:
-            encoded_data.append(1)
-            encoded_data.append(-1)
-    return encoded_data
+            manchester.append(-1)
+            manchester.append(1)
+    return manchester
 
 
-def manchester_decode(data):
-    decoded_data = []
-    for i in range(0, len(data), 2):
-        if data[i] == -1 and data[i+1] == 1:
-            decoded_data.append(0)
-        elif data[i] == 1 and data[i+1] == -1:
-            decoded_data.append(1)
-        else:
-            print("Invalid data")
-            return
-    return decoded_data
-
-
-def plot_graph(data, title):
-    x = np.arange(0, len(data))
-    y = data
-    plt.step(x, y, where='post')
-    plt.title(title)
-    plt.xlabel("Time")
-    plt.ylabel("Amplitude")
-    plt.ylim(-2, 2)
+def plot_graph(data, manchester):
+    x = np.arange(0, len(data), 0.5)
+    plt.plot(x, data, drawstyle='steps-pre', label='NRZ')
+    plt.plot(x, manchester, drawstyle='steps-pre', label='Manchester')
+    plt.legend()
     plt.show()
 
 
 def main():
-    data = list(map(int, input("Enter data: ").split()))
-    encoded_data = manchester_encode(data)
-    decoded_data = manchester_decode(encoded_data)
-    plot_graph(data, "Input data")
-    plot_graph(encoded_data, "Manchester encoded data")
-    plot_graph(decoded_data, "Manchester decoded data")
+    data = input("Enter data: ")
+    data = list(map(int, data))
+    manchester_data = manchester(data)
+    plot_graph(data, manchester_data)
