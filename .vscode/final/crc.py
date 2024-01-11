c255e2cb58a7d8909a9cd40d5caf6f401edae0a7
Akibur Rahman <me.akiburrahman@gmail.com>
2024-01-11 11:44:50 +0600

    Create crc.py

---
+++ .vscode/final/crc.py
@@ -0,0 +1,43 @@
+# write a program for verifying crc code using polynomial division method
+
+def xor(a, b):
+    result = []
+    for i in range(1, len(b)):
+        if a[i] == b[i]:
+            result.append('0')
+        else:
+            result.append('1')
+    return ''.join(result)
+
+
+def mod2div(divident, divisor):
+    pick = len(divisor)
+    tmp = divident[0: pick]
+    while pick < len(divident):
+        if tmp[0] == '1':
+            tmp = xor(divisor, tmp) + divident[pick]
+        else:
+            tmp = xor('0'*pick, tmp) + divident[pick]
+        pick += 1
+    if tmp[0] == '1':
+        tmp = xor(divisor, tmp)
+    else:
+        tmp = xor('0'*pick, tmp)
+    checkword = tmp
+    return checkword
+
+
+def encodeData(data, key):
+    l_key = len(key)
+    appended_data = data + '0'*(l_key-1)
+    remainder = mod2div(appended_data, key)
+    codeword = data + remainder
+    return codeword
+
+
+def decodeData(code, key):
+    remainder = mod2div(code, key)
+    return remainder
+
+
+data = "100100"
